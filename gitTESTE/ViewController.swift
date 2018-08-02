c8858f9aae5d647df852f7b416de73ba5a83b84e
Nickson Kley Gonçalves Da Silva <nicksonkley14@gmail.com>
2018-08-02 14:45:21 -0400

    Initial Commit

---
+++ gitTESTE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitTESTE
+//
+//  Created by Nickson Kley Gonçalves Da Silva on 02/08/2018.
+//  Copyright © 2018 Nickson Kley Gonçalves Da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
